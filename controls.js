a3ff4e8f5c095d0db83ae6873c1e978a6d238ef9
lgmohale <lgmohale@outlook.com>
2018-08-15 16:51:00 +0200

    added controls, ship and bullets

--- controls.js
+++ controls.js
@@ -4,13 +4,18 @@ class controls extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('space','assets/space.png');
+        this.load.image('space','assets/ship.png');
+        this.load.image('stars','assets/stars.jpg');
+        this.load.image('laser','assets/bullets.png');
     }
+    
+    
+
 
     create(){
         this.image = this.add.image(60,300, 'space');
 
-        this.input.keyboard.on('keyup_D', function(event){
+        this.input.keyboard.on('keydown_D', function(event){
             this.image.x +=10;
         },this);
 
@@ -18,12 +23,19 @@ class controls extends Phaser.Scene {
             this.image.x -=10;
         },this);
 
-        this.input.keyboard.on('keyup_S', function(event){
+        this.input.keyboard.on('keydown_S', function(event){
             this.image.y +=10;
         },this);
 
-        this.input.keyboard.on('keyup_W', function(event){
+        this.input.keyboard.on('keydown_W', function(event){
             this.image.y -=10;
         },this);
+
+        this.input.keyboard.on('keydown_P', function(event){
+            var physicsImage = this.physics.add.image(this.image.x, this.image.y, "laser");
+            physicsImage.setVelocity(2000, -75).setActive().setBounce(0);
+        },this)
+        
     }
+
 }
